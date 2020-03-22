e2af62ff106ebbfb2eea5c9edeefc9847256f938
Sam <Sam@Sam-PC>
2020-03-22 20:10:11 +0800

    Implements "GUI" mode ("com.lowagie.text.html.HtmlPdfWriter")

--- pdf-html-writer/src/main/java/com/lowagie/text/html/HtmlPdfWriter.java
+++ pdf-html-writer/src/main/java/com/lowagie/text/html/HtmlPdfWriter.java
@@ -1,22 +1,50 @@
 package com.lowagie.text.html;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Reader;
+import java.util.Objects;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextComponentUtil;
+import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfWriter;
 
-public class HtmlPdfWriter {
+public class HtmlPdfWriter implements ActionListener, InitializingBean {
+
+	private static final String WRAP = "wrap";
+
+	private static final int PREFERRED_WIDTH = 300;
 
 	private JFrame jFrame = null;
+
+	private JTextComponent tfOutput = null;
+
+	private AbstractButton btnExecute, btnCopy = null;
 
 	public JFrame getjFrame() {
 		return jFrame;
@@ -24,6 +52,106 @@ public class HtmlPdfWriter {
 
 	public void setjFrame(final JFrame jFrame) {
 		this.jFrame = jFrame;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		init(jFrame);
+	}
+
+	private void init(final Container container) {
+		//
+		add(container, new JLabel());
+		add(container, btnExecute = new JButton("Select HTML file"), WRAP);
+		//
+		add(container, new JLabel("Output"));
+		add(container, tfOutput = new JTextField());
+		tfOutput.setEditable(false);
+		tfOutput.setPreferredSize(new Dimension(PREFERRED_WIDTH, (int) tfOutput.getPreferredSize().getHeight()));
+		add(container, btnCopy = new JButton("Copy"), WRAP);
+		//
+		addActionListener(this, btnExecute, btnCopy);
+		//
+	}
+
+	private static void addActionListener(final ActionListener actionListener, final AbstractButton... buttons) {
+		//
+		AbstractButton button = null;
+		//
+		for (int i = 0; buttons != null && i < buttons.length; i++) {
+			//
+			if ((button = buttons[i]) == null) {
+				continue;
+			}
+			//
+			button.addActionListener(actionListener);
+			//
+		} // for
+			//
+	}
+
+	private static void add(final Container container, final Component component) {
+		if (container != null && component != null) {
+			container.add(component);
+		}
+	}
+
+	private static void add(final Container container, final Component component, final Object object) {
+		if (container != null && component != null) {
+			container.add(component, object);
+		}
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent evt) {
+		//
+		final Object source = evt != null ? evt.getSource() : null;
+		//
+		if (Objects.deepEquals(source, btnExecute)) {
+			//
+			final JFileChooser jfc = new JFileChooser(".");
+			//
+			if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				//
+				final File file = jfc.getSelectedFile();
+				final String fileName = file != null ? file.getName() : null;
+				//
+				File fileOutput = null;
+				if (StringUtils.lastIndexOf(fileName, '.') > 0) {
+					fileOutput = new File(StringUtils.substringBeforeLast(fileName, ".") + ".pdf");
+				} else {
+					fileOutput = new File(fileName + ".pdf");
+				}
+				//
+				try {
+					//
+					JTextComponentUtil.setText(tfOutput, null);
+					writeHtmlFileToPdfFile(jfc.getSelectedFile(), fileOutput);
+					JTextComponentUtil.setText(tfOutput, fileOutput.getAbsolutePath());
+					//
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+				//
+			} // if
+				//
+		} else if (Objects.deepEquals(source, btnCopy)) {
+			//
+			setContents(getSystemClipboard(Toolkit.getDefaultToolkit()),
+					new StringSelection(JTextComponentUtil.getText(tfOutput)), null);
+			//
+		}
+		//
+	}
+
+	private static Clipboard getSystemClipboard(final Toolkit instance) {
+		return instance != null ? instance.getSystemClipboard() : null;
+	}
+
+	private static void setContents(final Clipboard instance, final Transferable contents, final ClipboardOwner owner) {
+		if (instance != null && contents != null) {
+			instance.setContents(contents, owner);
+		}
 	}
 
 	public static void main(final String[] args) {
@@ -35,6 +163,7 @@ public class HtmlPdfWriter {
 		}
 		//
 		final String arg = args[0];
+		//
 		final File file = arg != null ? new File(arg) : null;
 		if (file == null || !file.exists()) {
 			throw new IllegalArgumentException("File not found,file=" + arg);
@@ -47,15 +176,28 @@ public class HtmlPdfWriter {
 			fileOutput = new File(arg + ".pdf");
 		}
 		//
-		try (final Document document = new Document(); final Reader reader = new FileReader(file)) {
-			PdfWriter.getInstance(document, new FileOutputStream(fileOutput));
-			// step 2: we open the document
-			document.open();
-			// step 3: parsing the HTML document to convert it in PDF
-			HtmlParser.parse(document, reader);
-		} catch (final DocumentException | IOException de) {
-			System.err.println(de.getMessage());
+		try {
+			writeHtmlFileToPdfFile(file, fileOutput);
+		} catch (final DocumentException | IOException e) {
+			e.printStackTrace();
 		}
+		//
+	}
+
+	private static void writeHtmlFileToPdfFile(final File input, final File output) throws IOException {
+		//
+		try (final Document document = new Document();
+				final Reader reader = input != null ? new FileReader(input) : null;
+				final OutputStream os = output != null ? new FileOutputStream(output) : null) {
+			//
+			PdfWriter.getInstance(document, os);
+			//
+			document.open();
+			//
+			HtmlParser.parse(document, reader);
+			//
+		} // try
+			//
 	}
 
 }
--- pdf-html-writer/src/test/java/com/lowagie/text/html/HtmlPdfWriterTtest.java
+++ pdf-html-writer/src/test/java/com/lowagie/text/html/HtmlPdfWriterTtest.java
@@ -1,18 +1,173 @@
 package com.lowagie.text.html;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.swing.AbstractButton;
+import javax.swing.JTextField;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.lowagie.text.ExceptionConverter;
 
 class HtmlPdfWriterTtest {
 
+	private static Method METHOD_WRITE_HTML_FILE_TO_PDF_FILE, METHOD_ADD_ACTION_LISTENER, METHOD_ADD2, METHOD_ADD3,
+			METHOD_GET_SYSTEM_CLIP_BOARD, METHOD_SET_CONTENTS = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = HtmlPdfWriter.class;
+		//
+		(METHOD_WRITE_HTML_FILE_TO_PDF_FILE = clz.getDeclaredMethod("writeHtmlFileToPdfFile", File.class, File.class))
+				.setAccessible(true);
+		//
+		(METHOD_ADD_ACTION_LISTENER = clz.getDeclaredMethod("addActionListener", ActionListener.class,
+				AbstractButton[].class)).setAccessible(true);
+		//
+		(METHOD_ADD2 = clz.getDeclaredMethod("add", Container.class, Component.class)).setAccessible(true);
+		//
+		(METHOD_ADD3 = clz.getDeclaredMethod("add", Container.class, Component.class, Object.class))
+				.setAccessible(true);
+		//
+		(METHOD_GET_SYSTEM_CLIP_BOARD = clz.getDeclaredMethod("getSystemClipboard", Toolkit.class)).setAccessible(true);
+		//
+		(METHOD_SET_CONTENTS = clz.getDeclaredMethod("setContents", Clipboard.class, Transferable.class,
+				ClipboardOwner.class)).setAccessible(true);
+		//
+	}
+
+	private Clipboard systemClipboard = null;
+
+	@BeforeEach
+	void beforeEach() throws Throwable {
+		systemClipboard = getSystemClipboard(Toolkit.getDefaultToolkit());
+	}
+
 	@Test
-	void tsetMain() {
+	void testAfterPropertiesSet() {
+		Assertions.assertDoesNotThrow(() -> new HtmlPdfWriter().afterPropertiesSet());
+	}
+
+	@Test
+	void testMain() {
 		//
 		Assertions.assertThrows(IllegalArgumentException.class, () -> HtmlPdfWriter.main(null));
 		Assertions.assertThrows(IllegalArgumentException.class, () -> HtmlPdfWriter.main(new String[0]));
 		Assertions.assertThrows(IllegalArgumentException.class, () -> HtmlPdfWriter.main(new String[] { null }));
 		Assertions.assertDoesNotThrow(() -> HtmlPdfWriter.main(new String[] { "." }));
 		//
+	}
+
+	@Test
+	void testWriteHtmlFileToPdfFile() {
+		Assertions.assertThrows(ExceptionConverter.class, () -> writeHtmlFileToPdfFile(null, null));
+	}
+
+	private static void writeHtmlFileToPdfFile(final File input, final File output) throws Throwable {
+		try {
+			METHOD_WRITE_HTML_FILE_TO_PDF_FILE.invoke(null, input, output);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testAddActionListener() {
+		//
+		Assertions.assertDoesNotThrow(() -> addActionListener(null, (AbstractButton[]) null));
+		Assertions.assertDoesNotThrow(() -> addActionListener(null, (AbstractButton) null));
+		//
+	}
+
+	private void addActionListener(final ActionListener actionListener, final AbstractButton... buttons)
+			throws Throwable {
+		try {
+			METHOD_ADD_ACTION_LISTENER.invoke(null, actionListener, buttons);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testAdd() {
+		//
+		final Container container = new Container();
+		Assertions.assertDoesNotThrow(() -> add(container, null));
+		Assertions.assertDoesNotThrow(() -> add(container, null, null));
+		//
+		final Component component = new JTextField();
+		Assertions.assertDoesNotThrow(() -> add(container, component));
+		Assertions.assertDoesNotThrow(() -> add(container, component, null));
+		//
+	}
+
+	private static void add(final Container container, final Component component) throws Throwable {
+		try {
+			METHOD_ADD2.invoke(null, container, component);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static void add(final Container container, final Component component, final Object object)
+			throws Throwable {
+		try {
+			METHOD_ADD3.invoke(null, container, component, object);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetSystemClipboard() throws Throwable {
+		Assertions.assertNull(getSystemClipboard(null));
+	}
+
+	private static Clipboard getSystemClipboard(final Toolkit instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_SYSTEM_CLIP_BOARD.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Clipboard) {
+				return (Clipboard) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testSetContents() {
+		//
+		Assertions.assertDoesNotThrow(() -> setContents(null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> setContents(systemClipboard, null, null));
+		Assertions.assertDoesNotThrow(() -> setContents(systemClipboard, new StringSelection(null), null));
+		//
+	}
+
+	private static void setContents(final Clipboard instance, final Transferable contents, final ClipboardOwner owner)
+			throws Throwable {
+		try {
+			METHOD_SET_CONTENTS.invoke(null, instance, contents, owner);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
 	}
 
 }
